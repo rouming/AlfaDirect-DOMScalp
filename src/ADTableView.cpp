a002dcdf59603a9384f472ab816660dea71b7474
rouming <work@work>
2011-11-07 15:28:12 +0400

    Identation tweaks

--- src/ADTableView.cpp
+++ src/ADTableView.cpp
@@ -4,27 +4,27 @@
 /***************************************************************************************/
 
 ADTableView::ADTableView ( QWidget* parent ) :
-	QTableView(parent),
-	m_adMainWindow(0)
+    QTableView(parent),
+    m_adMainWindow(0)
 {}
 
 void ADTableView::setADMainWindow ( ADMainWindow* wnd )
 {
-	m_adMainWindow = wnd;
+    m_adMainWindow = wnd;
 }
 
 void ADTableView::mousePressEvent ( QMouseEvent* me )
 {
-	if ( me && m_adMainWindow )
-		m_adMainWindow->mousePressEventADTableView(this, me);
-	QTableView::mousePressEvent( me );
+    if ( me && m_adMainWindow )
+        m_adMainWindow->mousePressEventADTableView(this, me);
+    QTableView::mousePressEvent( me );
 }
 
 void ADTableView::keyPressEvent ( QKeyEvent* ke )
 {
-	if ( ke && m_adMainWindow )
-		m_adMainWindow->keyPressEventADTableView(this, ke);
-	QTableView::keyPressEvent( ke );
+    if ( ke && m_adMainWindow )
+        m_adMainWindow->keyPressEventADTableView(this, ke);
+    QTableView::keyPressEvent( ke );
 }
 
 /***************************************************************************************/
